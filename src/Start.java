8e918b9d5e468a8cf9e3228f12d0caecbd0bd81c
SimoTede06 <s8901718r@majorana-giorgi.edu.it>
2024-11-25 09:21:15 +0100

    ciao 1 e 2
    ho aggiunto 2 stampe

--- src/Start.java
+++ src/Start.java
@@ -4,6 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//ciao
+		System.out.println("Ciao");
+		System.out.println("Ciao2");
 	}
 
 }
